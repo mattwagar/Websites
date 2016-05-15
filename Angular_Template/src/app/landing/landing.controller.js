38deea9403f6103b786b8afd9c872ac67d924f23
Matthew Wagar <mrw77@drexel.edu>
2016-05-15 19:20:07 -0400

    Angular Template
    I stripped a lot of parts from my Weather App into a simple angular
    template I can use for future projects

---
+++ Angular_Template/src/app/landing/landing.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.landing')
+        .controller('LandingController', LandingController);
+
+    LandingController.$inject = [];
+
+    function LandingController() {
+        var vm = this;
+
+    }
+
+})();
