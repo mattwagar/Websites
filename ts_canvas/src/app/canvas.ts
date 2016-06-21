842c7fbe4cfd243337ef1a21bd48819e2a19b918
Matthew Wagar <mrw77@drexel.edu>
2016-06-21 00:26:05 -0400

    rotating circles
    Created rotating circles with 2d Canvas. Each are random colors.

--- ts_canvas/src/app/canvas.ts
+++ ts_canvas/src/app/canvas.ts
@@ -1,32 +1,104 @@
-window.requestAnimFrame = function() {
-  return  window.requestAnimationFrame ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function(callback) {
-            window.setTimeout(callback, 1000/ 60);
-          };
-}();
+// requestAnim shim layer by Paul Irish
+var requestAnimFrame: (callback: () => void) => void = (function () {
+    return window.requestAnimationFrame ||
+        (<any>window).webkitRequestAnimationFrame ||
+        (<any>window).mozRequestAnimationFrame ||
+        (<any>window).oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60, new Date().getTime());
+        };
+})();
 
-window.onload = function() {
-  canv:HTMLCanvasElement = document.getElementById('canvas')
-  canv.width = 500;
-  canv.height = 500;
-  ctx = my_canvas.getContext("2d");
-  animate();
-};
+
+var canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, toggle;
+
+init();
+animate();
+
+function init() {
+
+    canvas = document.createElement('canvas');
+    canvas.width = 512;
+    canvas.height = 512;
+
+    ctx = canvas.getContext('2d');
+
+    document.body.appendChild(canvas);
+
+}
+
+function Circle(color: string, width: number, radius: number, start, end: number) {
+    ctx.beginPath();
+    ctx.arc(256, 256, radius, start, end)
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.stroke();
+
+}
 
 function animate() {
-  update();
-  draw();
-  requestAnimFrame(animate);
+    requestAnimFrame(animate);
+    draw();
+
 }
 
-function update(){
-  
+var colors1, colors2;
+
+
+function colorArr1() {
+    colors1 = new Array;
+
+    for (let i = 0; i < 6; i++) {
+        colors1.push(rngColor());
+    }
 }
 
-function draw(){
-    ctx.
+function colorArr2() {
+    colors2 = new Array;
+
+    for (let i = 0; i < 6; i++) {
+        colors2.push(rngColor());
+    }
 }
+
+colorArr1();
+colorArr2();
+
+
+function draw() {
+
+
+    ctx.clearRect(0, 0, 512, 512);
+    ctx.translate(256, 256);
+    ctx.rotate(2 * Math.PI / 180);
+    ctx.translate(-256, -256);
+
+
+    let i = 1;
+    for (let color in colors1) {
+        Circle(colors1[color], 25, 30 * i, 0, 0.5 * Math.PI);
+        Circle(colors1[color], 25, 30 * i, 1 * Math.PI, 1.5 * Math.PI);
+        i++;
+    }
+
+    i = 1;
+    for (let color in colors2) {
+        Circle(colors2[color], 25, 30 * i, 0.5 * Math.PI, 1 * Math.PI);
+        Circle(colors2[color], 25, 30 * i, 1.5 * Math.PI, 2 * Math.PI);
+        i++;
+    }
+
+}
+
+
+function rngColor() {
+
+    let r = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+    let g = Math.round(Math.random() * 255);
+
+    return ("rgb(" + r + "," + g + "," + b + ")");
+
+}
+
--- ts_canvas/src/js/canvas.js
+++ ts_canvas/src/js/canvas.js
@@ -1,30 +1,73 @@
-window.requestAnimFrame = function () {
+// requestAnim shim layer by Paul Irish
+var requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback) {
-            window.setTimeout(callback, 1000 / 60);
+            window.setTimeout(callback, 1000 / 60, new Date().getTime());
         };
-}();
-window.onload = function () {
-    canv: HTMLCanvasElement = document.getElementById('canvas');
-    canv.width = 500;
-    canv.height = 500;
-    ctx = my_canvas.getContext("2d");
-    animate();
-};
+})();
+var canvas, ctx, toggle;
+init();
+animate();
+function init() {
+    canvas = document.createElement('canvas');
+    canvas.width = 512;
+    canvas.height = 512;
+    ctx = canvas.getContext('2d');
+    document.body.appendChild(canvas);
+}
+function Circle(color, width, radius, start, end) {
+    ctx.beginPath();
+    ctx.arc(256, 256, radius, start, end);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.stroke();
+}
 function animate() {
-    update();
-    draw();
     requestAnimFrame(animate);
+    draw();
 }
-function update() {
+var colors1, colors2;
+function colorArr1() {
+    colors1 = new Array;
+    for (var i = 0; i < 6; i++) {
+        colors1.push(rngColor());
+    }
 }
+function colorArr2() {
+    colors2 = new Array;
+    for (var i = 0; i < 6; i++) {
+        colors2.push(rngColor());
+    }
+}
+colorArr1();
+colorArr2();
 function draw() {
-    ctx.
-    ;
+    ctx.clearRect(0, 0, 512, 512);
+    ctx.translate(256, 256);
+    ctx.rotate(2 * Math.PI / 180);
+    ctx.translate(-256, -256);
+    var i = 1;
+    for (var color in colors1) {
+        Circle(colors1[color], 25, 30 * i, 0, 0.5 * Math.PI);
+        Circle(colors1[color], 25, 30 * i, 1 * Math.PI, 1.5 * Math.PI);
+        i++;
+    }
+    i = 1;
+    for (var color in colors2) {
+        Circle(colors2[color], 25, 30 * i, 0.5 * Math.PI, 1 * Math.PI);
+        Circle(colors2[color], 25, 30 * i, 1.5 * Math.PI, 2 * Math.PI);
+        i++;
+    }
+}
+function rngColor() {
+    var r = Math.round(Math.random() * 255);
+    var b = Math.round(Math.random() * 255);
+    var g = Math.round(Math.random() * 255);
+    return ("rgb(" + r + "," + g + "," + b + ")");
 }
 
 //# sourceMappingURL=canvas.js.map
