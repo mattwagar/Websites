17ca854dbbb07b597380c1944f9dc43fd24c8543
Matthew Wagar <mrw77@drexel.edu>
2016-06-22 21:28:55 -0400

    random path, random color
    Created a simple random path, random color canvas piece. uses typescript
    class "app" to draw onto canvas.

--- ts_canvas/src/js/falling.js
+++ ts_canvas/src/js/falling.js
@@ -1,85 +1,84 @@
-var requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 1000 / 60, new Date().getTime());
-        };
-})();
-var canvas, ctx;
-init();
-animate();
-function init() {
-    canvas = document.createElement('canvas');
-    canvas.width = 512;
-    canvas.height = 512;
-    ctx = canvas.getContext('2d');
-    document.body.appendChild(canvas);
-}
-function animate() {
-    requestAnimFrame(animate);
-    draw();
-}
 var Circle = (function () {
-    function Circle(x, y, color, width, radius, start, end) {
+    function Circle(app, x, y, fillcolor, strokecolor, width, radius, start, end) {
+        this.app = app;
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.fillcolor = fillcolor;
+        this.strokecolor = strokecolor;
         this.width = width;
         this.radius = radius;
         this.start = start;
         this.end = end;
     }
-    Circle.prototype.d = function () {
+    Circle.prototype.draw = function () {
+        var ctx = this.app.ctx;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, this.start, this.end);
-        ctx.strokeStyle = this.color;
+        ctx.strokeStyle = this.strokecolor;
         ctx.lineWidth = this.width;
         ctx.stroke();
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = this.fillcolor;
         ctx.fill();
-    };
-    Circle.prototype.drop = function () {
-        this.x -= 1;
     };
     return Circle;
 }());
-var c1 = new Circle(256, 90, 'blue', 20, 60, 0, 2 * Math.PI);
-var c2 = new Circle(110, 256, 'pink', 20, 90, 0, 2 * Math.PI);
-c1.d();
-c2.d();
-console.log(c1.y);
-c1.y = 30;
-c1.d();
-function draw() {
-    ctx.beginPath();
-    ctx.arc(256, 256, 30, 0, Math.PI * 2);
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 20;
-    ctx.stroke();
-}
-// class Gravity {
-//     speed: number;
-//     constructor(speed: number) {
-//         this.speed = speed;
-//     }
-//     fall() {
-//     }
-// }
-// class Shape extends Gravity {
-//     constructor(speed: number) {
-//         super(speed);
-//     }
-//     Circle(color: string, width: number, radius: number, start: number, end: number):void {
-//         ctx.beginPath();
-//         ctx.arc(256, 256, radius, start, end)
-//         ctx.strokeStyle = color;
-//         ctx.lineWidth = width;
-//         ctx.stroke();
-//         ctx.fill();
-//     }
-// }
+var App = (function () {
+    function App() {
+        var _this = this;
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.sizeCanvas();
+        this.initEvents();
+        window.requestAnimationFrame(function (t) { _this.draw(t); });
+        console.log(this);
+        this.arr = new Array;
+        for (var i = 0; i < 50; i++) {
+            this.arr.push(new Circle(this, this.ctx.canvas.width / 2, this.ctx.canvas.height / 2, this.rngColor(), this.rngColor(), 10, this.randomSize(), 0, 2 * Math.PI));
+        }
+    }
+    App.prototype.sizeCanvas = function () {
+        this.w = this.ctx.canvas.width = window.innerWidth;
+        this.h = this.ctx.canvas.height = window.innerHeight;
+    };
+    App.prototype.draw = function (t) {
+        var _this = this;
+        window.requestAnimationFrame(function (t) { _this.draw(t); });
+        for (var i in this.arr) {
+            this.arr[i].draw();
+            this.arr[i].x += this.randomPath() * 15;
+            this.arr[i].y += this.randomPath() * 15;
+            if (this.arr[i].x > this.ctx.canvas.width - 15) {
+                this.arr[i].x = this.ctx.canvas.width - 15;
+            }
+            else if (this.arr[i].x < 15) {
+                this.arr[i].x = 15;
+            }
+            if (this.arr[i].y > this.ctx.canvas.height - 15) {
+                this.arr[i].y = this.ctx.canvas.height - 15;
+            }
+            else if (this.arr[i].y < 15) {
+                this.arr[i].y = 15;
+            }
+        }
+    };
+    App.prototype.initEvents = function () {
+        var _this = this;
+        window.onresize = function (e) { _this.sizeCanvas(); };
+    };
+    App.prototype.randomPath = function () {
+        return (Math.random() * 2 - 1);
+    };
+    App.prototype.randomSize = function () {
+        return (Math.random() * 60);
+    };
+    App.prototype.rngColor = function () {
+        var r = Math.round(Math.random() * 255);
+        var b = Math.round(Math.random() * 255);
+        var g = Math.round(Math.random() * 255);
+        return ('rgb(' + r + ',' + g + ',' + b + ')');
+    };
+    return App;
+}());
+var app = new App();
 
 //# sourceMappingURL=falling.js.map
